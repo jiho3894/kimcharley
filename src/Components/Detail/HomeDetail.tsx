fa16850b392d8fe765ea5eb1cbfc5f081db3be2b
unknown <crsn1111@gmail.com>
2022-01-21 18:36:45 +0900

    HomeDetail

--- src/Components/Detail/HomeDetail.tsx
+++ src/Components/Detail/HomeDetail.tsx
@@ -51,7 +51,7 @@ const Banner = styled.div`
   background: linear-gradient(
     rgba(0, 0, 0, 0.7),
     rgba(0, 0, 0, 0.3),
-    rgba(0, 0, 0, 0.5)
+    rgba(0, 0, 0, 0.6)
   );
   position: absolute;
   top: 0;
